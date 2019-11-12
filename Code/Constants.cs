d945200f3a9d600eb87c553b4ab24cb36b2e5bb9
KnightsUCF <peterk@knights.ucf.edu>
2019-11-12 10:42:32 -0500

    Create Constants.cs

---
+++ Code/Constants.cs
@@ -0,0 +1,16 @@
+
+
+
+public static class Constants
+{
+    public static readonly string PlayerTag = "Player";
+    public static readonly string AnimationStarted = "started";
+    public static readonly string AnimationJump = "jump";
+    public static readonly string WidePathBorderTag = "WidePathBorder";
+
+    public static readonly string StatusTapToStart = "Tap to start";
+    public static readonly string StatusDeadTapToStart = "GAME OVER. Tap to start";
+
+    public static readonly string StatusTest = "Status test";
+}
+
