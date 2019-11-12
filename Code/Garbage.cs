06302aa944542f03b92f49e29d4435c1281bca2b
KnightsUCF <peterk@knights.ucf.edu>
2019-11-12 10:44:15 -0500

    Update Garbage.cs

--- Code/Garbage.cs
+++ Code/Garbage.cs
@@ -8,16 +8,58 @@ using UnityEngine;
 public class Garbage : MonoBehaviour
 {
 
-	void Start()
-	{
-		Invoke("DestroyObject", LifeTime);
-	}
 
-	void DestroyObject()
-	{
-		if (Game.Instance.GameState != GameState.Dead)
-			Destroy(gameObject);
-	}
+    public float LifeTime = 10f;
 
-	public float LifeTime = 10f;
+
+    bool playerOnTile;
+
+
+
+
+
+    void Start()
+    {
+        Invoke("DestroyObject", LifeTime);
+    }
+
+
+
+    void DestroyObject()
+    {
+        if (Game.Instance.GameState != GameState.Dead)
+        {
+            if (!playerOnTile) 
+            {
+                Debug.Log("Destroying game object");
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    
+
+    // detect whether the player is in the collider so we don't destroy the tile the player is resting on
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == Constants.PlayerTag)
+        {
+            playerOnTile = true;
+        }
+    }
+
+
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject.tag == Constants.PlayerTag)
+        {
+            playerOnTile = false;
+        }
+
+    }
+
+
+
 }
