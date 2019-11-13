c2df55a8d00bb40f4a4b0a6087fa466982db4cbb
KnightsUCF <peterk@knights.ucf.edu>
2019-11-13 18:42:33 -0500

    Update Controls.cs

--- Code/Controls.cs
+++ Code/Controls.cs
@@ -23,23 +23,66 @@ public class Controls : MonoBehaviour
 	// https://answers.unity.com/questions/29751/gradually-moving-an-object-up-to-speed-rather-then.html
 
 	public float speed = 0.0f;
-	public float maxSpeed = 10.0f; 
+	 
 	public float acceleration = 30.0f;
+    public float mobileAcceleration = 100.0f;
+
 	public float deceleration = 10.0f;
 	public float breakDeceleration = 50.0f;
 
+    public float breakMobileDeceleration = 100.0f;
+
     // turning
 
-	public float turnSpeed = 10.0f;
+	public float pcTurnSpeed = 10.0f;
+    public float mobileTurnSpeed = 50.0f;
+
 	public float turnLeftBoundary = 0.0f;
 	public float turnRightBoundary = 0.0f;
 
-    private float maxCruiseSpeed = 20.0f;// should be zero
+    public float maxSpeed = 50.0f; // was 10 before with maxCruiseSpeed being 20
+    private float maxCruiseSpeed = 50.0f;// should be zero
 
 
 	Vector3 pos;
 
 
+    // mobile touch controls
+
+    private Vector3 fp;   //First touch position
+    private Vector3 lp;   //Last touch position
+
+    private float dragDistance;  //minimum distance for a swipe to be registered
+
+
+    // we split off the velocity setting mechanism from Move() to make the code clear
+    // Move() just allows us to set this speed with the user interface
+
+
+
+    void SetPos()
+    {
+
+
+        // transform.position.x = transform.position.x + speed * Time.deltaTime;
+        pos = transform.position;
+        // pos.x = transform.position.x + speed * Time.deltaTime;
+
+        // we're taking out cruise for now...
+        // pos.x = transform.position.x + maxCruiseSpeed * Time.deltaTime;
+
+        // really bad way of adding a max speed:
+        pos.x = transform.position.x + maxSpeed * Time.deltaTime;
+
+        transform.position = pos;
+    }
+
+
+    void SetMaxSpeed()
+    {
+        if (speed > maxCruiseSpeed) maxCruiseSpeed = speed;
+        if (speed > maxSpeed) speed = maxSpeed;
+    }
 
 
 
@@ -62,26 +105,25 @@ public class Controls : MonoBehaviour
 
         // Debug.Log("speed: " + speed);
         // Debug.Log("maxCruiseSpeed: " + maxCruiseSpeed);
-        if (speed > maxCruiseSpeed) maxCruiseSpeed = speed;
-
-        if (speed > maxSpeed) speed = maxSpeed;
         
 
-		// transform.position.x = transform.position.x + speed * Time.deltaTime;
-		pos = transform.position;
-        // pos.x = transform.position.x + speed * Time.deltaTime;
-
-        // we're taking out cruise for now...
-        // pos.x = transform.position.x + maxCruiseSpeed * Time.deltaTime;
-
-        // really bad way of adding a max speed:
-        pos.x = transform.position.x + maxSpeed * Time.deltaTime;
-
-
-
-        transform.position = pos;
-
 	}
+
+
+    void TurnLeft(float turnSpeed)
+    {
+        pos = transform.position;
+        pos.z = transform.position.z + turnSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+
+    void TurnRight(float turnSpeed)
+    {
+        pos = transform.position;
+        pos.z = transform.position.z - turnSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
 
 
 
@@ -89,16 +131,12 @@ public class Controls : MonoBehaviour
 	{
 		if (Input.GetKey(KeyCode.A))
 		{
-			pos = transform.position;
-			pos.z = transform.position.z + turnSpeed * Time.deltaTime;
-			transform.position = pos;
+            TurnLeft(pcTurnSpeed);
 		}
 
         if (Input.GetKey(KeyCode.D))
 		{
-			pos = transform.position;
-			pos.z = transform.position.z - turnSpeed * Time.deltaTime;
-			transform.position = pos;
+            TurnRight(pcTurnSpeed);
 		}
 	}
 
@@ -118,13 +156,41 @@ public class Controls : MonoBehaviour
     }
 
 
+    void MobileBreak()
+    {
+        // we don't need this if statement if we are going to turn this in the mobile swipe  up / down condition
+        // this one should go into swipe down
+
+        // if (Input.GetKey(KeyCode.LeftShift))
+        
+        speed = speed - breakMobileDeceleration * Time.deltaTime;
+        
+
+        /* This stuff is already taken care of in regular Break() in the Update method
+         
+        if (speed <= 0) speed = 0; // so we don't go backwards with the break
+
+        pos = transform.position;
+        pos.x = transform.position.x + speed * Time.deltaTime;
+        transform.position = pos;
+        */
+    }
+
+
 
 	void RunMovementRoutines()
 	{
-		Move();
+		Move(); // changes the speed
+
+        SetMaxSpeed();
+
 		Turn();
+
         Break();
-	}
+
+        SetPos(); // applies all the speed variables to actually move the car transform position
+
+    }
 
 
     // UI ////////////////////////////////////////////////////////////
@@ -139,15 +205,113 @@ public class Controls : MonoBehaviour
 
 
     //////////////////////////////////////////////////////////////////
-    
 
 
 
-	void Update()
+    // TOUCH CONTROLS ////////////////////////////////////////////////
+
+
+    void ProcessTouches()
+    {
+        if (Input.touchCount == 1) // user is touching the screen with a single touch
+        {
+            Touch touch = Input.GetTouch(0); // get the touch
+            if (touch.phase == TouchPhase.Began) //check for the first touch
+            {
+                fp = touch.position;
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Moved) // update the last position based on where they moved
+            {
+                lp = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Ended) //check if the finger is removed from the screen
+            {
+                lp = touch.position;  //last touch position. Ommitted if you use list
+
+                //Check if drag distance is greater than 20% of the screen height
+                if (Mathf.Abs(lp.x - fp.x) > dragDistance || Mathf.Abs(lp.y - fp.y) > dragDistance)
+                {//It's a drag
+                 //check if the drag is vertical or horizontal
+                    if (Mathf.Abs(lp.x - fp.x) > Mathf.Abs(lp.y - fp.y))
+                    {   //If the horizontal movement is greater than the vertical movement...
+                        if ((lp.x > fp.x))  //If the movement was to the right)
+                        {   
+                            // Right swipe
+
+                            Debug.Log("Right Swipe");
+                            // hudLog.UpdateText("Right Swipe");
+                            
+                            TurnRight(mobileTurnSpeed);
+
+                        }
+                        else
+                        {   
+                            // Left swipe
+
+                            Debug.Log("Left Swipe");
+                            // hudLog.UpdateText("Left Swipe");
+
+                            TurnLeft(mobileTurnSpeed);
+                        }
+                    }
+
+                    else
+                    {   //the vertical movement is greater than the horizontal movement
+                        if (lp.y > fp.y && (speed < maxSpeed))  //If the movement was up
+                        {   
+                            // Up swipe - speed up
+
+                            Debug.Log("Up Swipe");
+                            // hudLog.UpdateText("Up Swipe");
+                            
+                            speed = speed + mobileAcceleration * Time.deltaTime;
+                        }
+
+                        else
+                        {   //Down swipe
+                            Debug.Log("Down Swipe");
+                            // hudLog.UpdateText("Down Swipe");
+                            MobileBreak();
+                        }
+                    }
+                }
+                else
+                {   //It's a tap as the drag distance is less than 20% of the screen height
+                    Debug.Log("Tap");
+                    // hudLog.UpdateText("Tap");
+                }
+            }
+        }
+    }
+
+
+
+    //////////////////////////////////////////////////////////////////
+
+
+    void Start()
+    {
+        // mobile
+
+        dragDistance = Screen.height * 15 / 100; //dragDistance is 15% height of the screen
+    }
+
+
+
+    void Update()
 	{
+        // PC
+
 		RunMovementRoutines();
 
         CheckForClose();
+
+
+        // mobile 
+
+        ProcessTouches();
+
 
 
         UI.Instance.SetStatus(speed.ToString());
